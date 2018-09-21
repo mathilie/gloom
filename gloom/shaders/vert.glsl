a9e9693049c41657f0e503c8f0c86932d157b311
mathilie <mlien1995@gmail.com>
2018-09-21 12:09:11 +0200

    Added task 3

--- gloom/shaders/vert.glsl
+++ gloom/shaders/vert.glsl
@@ -2,6 +2,7 @@
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec4 vertexColour;
 layout(location = 1) out vec4 outVertexColour;
+layout(location = 3) mat4 transformationMatrix
 void main()
 {
 	outVertexColour = vertexColour;
--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -4,8 +4,12 @@
 GLuint array;
 GLuint buffer;
 GLuint colorBuffer;
+GLfloat identity[4];
 
 void makeAndLoadVertexes(GLfloat coords[], size_t lengthCoords, GLfloat colors[], size_t colorsLength);
+
+
+//TASK 1
 
 void task1() {
 	static const GLfloat coords[] = {
@@ -63,6 +67,9 @@ void drawTask1() {
 	glDrawArrays(GL_TRIANGLES, 0, 15);
 }
 
+
+//TASK 2
+
 void task2() {
 	static const GLfloat coords[] = {
 		-1.0f, -1.0f, -0.5f,
@@ -108,6 +115,63 @@ void drawTask2() {
 	glDrawArrays(GL_TRIANGLES, 3, 3);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
+
+
+//Task 3
+void task3() {
+	static const GLfloat coords[] = {
+		-1.0f, -1.0f, -0.5f,
+		1.0f, -1.0f, -0.5f,
+		0.0f , -0.2f , -0.5f,
+		-0.7f, -1.0f, 0.0f,
+		0.7f, -1.0f, 0.0f,
+		0.0f , 0.4f , 0.0f,
+		-0.4f, -1.0f, 0.5f,
+		0.4f, -1.0f, 0.5f,
+		0.0f , 0.9f , 0.5f };
+	static const GLfloat colors[]{
+		1.0f, 0.0f, 0.0f, 0.3f,
+		1.0f, 0.0f, 0.0f, 0.3f,
+		1.0f, 0.0f, 0.0f, 0.3f,
+		0.0f, 1.0f, 0.0f, 0.5f,
+		0.0f, 1.0f, 0.0f, 0.5f,
+		0.0f, 1.0f, 0.0f, 0.5f,
+		0.0f, 0.0f, 1.0f, 0.8f,
+		0.0f, 0.0f, 1.0f, 0.8f,
+		0.0f, 0.0f, 1.0f, 0.8f
+	};
+
+	identity[0] = { 1.0f, 0.0f, 0.0f, 0.0f };
+	identity[1] = { 0.0f, 1.0f, 0.0f, 0.0f };
+	identity[2] = { 0.0f, 0.0f, 1.0f, 0.0f };
+	identity[3] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+	glGenVertexArrays(1, &array);
+	glBindVertexArray(array);
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(coords), coords, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &colorBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+}
+
+void drawTask3() {
+	glBindVertexArray(array);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, NULL);
+	glUniform4fv(3, 1, indentity);
+	glDrawArrays(GL_TRIANGLES, 6, 3);
+	glDrawArrays(GL_TRIANGLES, 3, 3);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+
 void runProgram(GLFWwindow* window)
 {
     // Enable depth (Z) buffer (accept "closest" fragment)
@@ -136,7 +200,6 @@ void runProgram(GLFWwindow* window)
 
         // Draw your scene here
 		drawTask2();
-
         // Handle other events
         glfwPollEvents();
         handleKeyboardInput(window);
