3749472b734f587ab5cfb496187d54ae2431207f
mathilie <mlien1995@gmail.com>
2018-09-21 17:09:41 +0200

    Working on task 4

--- gloom/shaders/vert.glsl
+++ gloom/shaders/vert.glsl
@@ -1,10 +1,11 @@
 #version 430 core
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec4 vertexColour;
+layout(location = 2) uniform mat4 imat;
 layout(location = 1) out vec4 outVertexColour;
-layout(location = 3) mat4 transformationMatrix
 void main()
 {
 	outVertexColour = vertexColour;
-    gl_Position = vec4(position, 1.0f);
+	vec4 position4 = vec4(position, 1.0f);
+    gl_Position = transpose(imat)*position4;
 }
--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -1,14 +1,29 @@
 // Local headers
 #include "program.hpp"
 #include "gloom/gloom.hpp"
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/vec3.hpp>
+#include <glm/gtc/matrix_transform.hpp>#include <math.h>#include <glm/gtx/rotate_vector.hpp>#include <glm/gtx/rotate_normalized_axis.hpp>
 GLuint array;
 GLuint buffer;
 GLuint colorBuffer;
-GLfloat identity[4];
+GLfloat transmatrix[] = {
+	1.0f, 0.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f, 0.0f,
+	0.0f, 0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 0.0f, 1.0f
+};
+GLfloat view[] = {
+	1.0f, 0.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f, 0.0f,
+	0.0f, 0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 0.0f, 1.0f
+};
 
-void makeAndLoadVertexes(GLfloat coords[], size_t lengthCoords, GLfloat colors[], size_t colorsLength);
 
-
+/*
 //TASK 1
 
 void task1() {
@@ -29,21 +44,21 @@ void task1() {
 		0.0f, -0.9f, 0.0f,
 		-0.1f , 0.9f , 0.0f };
 	static const GLfloat colors[]{
-		1.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f,
-		0.5f, 0.5f, 0.5f,
-		0.6f, 0.4f, 0.2f,
-		0.4f, 0.8f, 0.1f,
-		0.0f, 0.0f, 0.0f,
-		0.2f, 0.2f, 0.2f,
-		0.8f, 0.8f, 0.8f,
-		0.1f, 0.5f, 0.7f,
-		1.0f, 0.0f, 1.0f,
-		1.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f
+		1.0f, 0.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f, 1.0f,
+		1.0f, 0.0f, 0.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 1.0f,
+		0.6f, 0.4f, 0.2f, 1.0f,
+		0.4f, 0.8f, 0.1f, 1.0f,
+		0.0f, 0.0f, 0.0f, 1.0f,
+		0.2f, 0.2f, 0.2f, 1.0f,
+		0.8f, 0.8f, 0.8f, 1.0f,
+		0.1f, 0.5f, 0.7f, 1.0f,
+		1.0f, 0.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f, 1.0f
 	};
 	glGenVertexArrays(1, &array);
 	glBindVertexArray(array);
@@ -72,15 +87,15 @@ void drawTask1() {
 
 void task2() {
 	static const GLfloat coords[] = {
-		-1.0f, -1.0f, -0.5f,
-		1.0f, -1.0f, -0.5f,
-		0.0f , -0.2f , -0.5f,
+		-1.0f, -1.0f, 0.8f,
+		1.0f, -1.0f, 0.8f,
+		0.0f , -0.2f , 0.8f,
 		-0.7f, -1.0f, 0.0f,
 		0.7f, -1.0f, 0.0f,
 		0.0f , 0.4f , 0.0f,
-		-0.4f, -1.0f, 0.5f,
-		0.4f, -1.0f, 0.5f,
-		0.0f , 0.9f , 0.5f };
+		-0.4f, -1.0f, -0.08,
+		0.4f, -1.0f, -0.8f,
+		0.0f , 0.9f , -0.8f };
 	static const GLfloat colors[]{
 		1.0f, 0.0f, 0.0f, 0.3f,
 		1.0f, 0.0f, 0.0f, 0.3f,
@@ -111,11 +126,11 @@ void drawTask2() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, NULL);
-	glDrawArrays(GL_TRIANGLES, 6, 3);
-	glDrawArrays(GL_TRIANGLES, 3, 3);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 0, 15);
+	//glDrawArrays(GL_TRIANGLES, 3, 3);
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
 }
-
+*/
 
 //Task 3
 void task3() {
@@ -141,10 +156,6 @@ void task3() {
 		0.0f, 0.0f, 1.0f, 0.8f
 	};
 
-	identity[0] = { 1.0f, 0.0f, 0.0f, 0.0f };
-	identity[1] = { 0.0f, 1.0f, 0.0f, 0.0f };
-	identity[2] = { 0.0f, 0.0f, 1.0f, 0.0f };
-	identity[3] = { 0.0f, 0.0f, 0.0f, 1.0f };
 
 	glGenVertexArrays(1, &array);
 	glBindVertexArray(array);
@@ -160,16 +171,73 @@ void task3() {
 }
 
 void drawTask3() {
+	const GLfloat transMatrix[]= {
+		1.0f, 0.0f, 0.0f, 0.5f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	};
 	glBindVertexArray(array);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, NULL);
-	glUniform4fv(3, 1, indentity);
+	glUniformMatrix4fv(2, 1, GL_FALSE, transMatrix);
 	glDrawArrays(GL_TRIANGLES, 6, 3);
 	glDrawArrays(GL_TRIANGLES, 3, 3);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
+
+//Task 4
+void task4() {
+	static const GLfloat coords[] = {
+		-1.0f, -1.0f, 0.5f,
+		1.0f, -1.0f, 0.5f,
+		0.0f , -0.2f , 0.5f,
+		-0.7f, -1.0f, 0.0f,
+		0.7f, -1.0f, 0.0f,
+		0.0f , 0.4f , 0.0f,
+		-0.4f, -1.0f, -0.5f,
+		0.4f, -1.0f, -0.5f,
+		-0.0f , 0.9f , -0.5f };
+	static const GLfloat colors[]{
+		1.0f, 0.0f, 0.0f, 0.3f,
+		1.0f, 0.0f, 0.0f, 0.3f,
+		1.0f, 0.0f, 0.0f, 0.3f,
+		0.0f, 1.0f, 0.0f, 0.5f,
+		0.0f, 1.0f, 0.0f, 0.5f,
+		0.0f, 1.0f, 0.0f, 0.5f,
+		0.0f, 0.0f, 1.0f, 0.8f,
+		0.0f, 0.0f, 1.0f, 0.8f,
+		0.0f, 0.0f, 1.0f, 0.8f
+	};
+
+	glGenVertexArrays(1, &array);
+	glBindVertexArray(array);
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(coords), coords, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &colorBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+}
+
+void drawTask4() {
+	glm::mat4x4 persMatrix = glm::perspective(3.14f / 2.0f, 1.0f, 0.5f, 10000.0f);
+	glm::mat4x4 transformation = glm::make_mat4x4(transmatrix);
+	transformation = glm::rotate(0.01f,0.0f,1.0f,0.0f)*transformation*persMatrix;
+	glBindVertexArray(array);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, NULL);
+	glUniformMatrix4fv(2, 1, GL_FALSE, glm::value_ptr(transformation));
+	glDrawArrays(GL_TRIANGLES, 0, 15);
+}
+
 
 
 void runProgram(GLFWwindow* window)
@@ -188,7 +256,7 @@ void runProgram(GLFWwindow* window)
 
 
     // Set up your scene here (create Vertex Array Objects, etc.)
-	task2();
+	task4();
 
 
 	//makeAndLoadVertexes(coords, 75, colors, 75);
@@ -199,7 +267,7 @@ void runProgram(GLFWwindow* window)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Draw your scene here
-		drawTask2();
+		drawTask4();
         // Handle other events
         glfwPollEvents();
         handleKeyboardInput(window);
@@ -210,9 +278,6 @@ void runProgram(GLFWwindow* window)
     }
 }
 
-void makeAndLoadVertexes(GLfloat* coords, size_t lengthCoords, GLfloat colors[], size_t colorsLength) {
-
-}
 
 
 void handleKeyboardInput(GLFWwindow* window)
@@ -222,4 +287,37 @@ void handleKeyboardInput(GLFWwindow* window)
     {
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
+
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+	{
+		transmatrix[7] += -0.01f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+	{
+		transmatrix[7] += 0.01f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+	{
+		transmatrix[3] += 0.01f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+	{
+		transmatrix[3] += -0.01f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		transmatrix[11] += 0.01f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		transmatrix[11] += -0.01f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		transmatrix[11] += -0.01f;
+	}
 }
