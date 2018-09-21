34f099a700284b8b82c045967c239aaa75f29662
mathilie <mlien1995@gmail.com>
2018-09-21 21:51:28 +0200

    Final Delivery

--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -5,7 +5,10 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/transform.hpp>
 #include <glm/vec3.hpp>
-#include <glm/gtc/matrix_transform.hpp>#include <math.h>#include <glm/gtx/rotate_vector.hpp>#include <glm/gtx/rotate_normalized_axis.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <math.h>
+
 GLuint array;
 GLuint buffer;
 GLuint colorBuffer;
@@ -15,12 +18,8 @@ GLfloat transmatrix[] = {
 	0.0f, 0.0f, 1.0f, 0.0f,
 	0.0f, 0.0f, 0.0f, 1.0f
 };
-GLfloat view[] = {
-	1.0f, 0.0f, 0.0f, 0.0f,
-	0.0f, 1.0f, 0.0f, 0.0f,
-	0.0f, 0.0f, 1.0f, 0.0f,
-	0.0f, 0.0f, 0.0f, 1.0f
-};
+GLfloat cameraPosition[] = { 0.0f, 0.0f, -1.0f };
+GLfloat cameraAngle[] = {0.0f,0.0f};
 
 
 /*
@@ -172,7 +171,7 @@ void task3() {
 
 void drawTask3() {
 	const GLfloat transMatrix[]= {
-		1.0f, 0.0f, 0.0f, 0.5f,
+		1.0f, 0.0f, 0.0f, 0.0f,
 		0.0f, 1.0f, 0.0f, 0.0f,
 		0.0f, 0.0f, 1.0f, 0.0f,
 		0.0f, 0.0f, 0.0f, 1.0f
@@ -201,17 +200,18 @@ void task4() {
 		0.4f, -1.0f, -0.5f,
 		-0.0f , 0.9f , -0.5f };
 	static const GLfloat colors[]{
-		1.0f, 0.0f, 0.0f, 0.3f,
-		1.0f, 0.0f, 0.0f, 0.3f,
-		1.0f, 0.0f, 0.0f, 0.3f,
+		1.0f, 0.0f, 0.0f, 0.5f,
+		1.0f, 0.0f, 0.0f, 0.5f,
+		1.0f, 0.0f, 0.0f, 0.5f,
 		0.0f, 1.0f, 0.0f, 0.5f,
 		0.0f, 1.0f, 0.0f, 0.5f,
 		0.0f, 1.0f, 0.0f, 0.5f,
-		0.0f, 0.0f, 1.0f, 0.8f,
-		0.0f, 0.0f, 1.0f, 0.8f,
-		0.0f, 0.0f, 1.0f, 0.8f
+		0.0f, 0.0f, 1.0f, 0.5f,
+		0.0f, 0.0f, 1.0f, 0.5f,
+		0.0f, 0.0f, 1.0f, 0.5f
 	};
-
+
+
 	glGenVertexArrays(1, &array);
 	glBindVertexArray(array);
 	glEnableVertexAttribArray(0);
@@ -226,16 +226,36 @@ void task4() {
 }
 
 void drawTask4() {
-	glm::mat4x4 persMatrix = glm::perspective(3.14f / 2.0f, 1.0f, 0.5f, 10000.0f);
-	glm::mat4x4 transformation = glm::make_mat4x4(transmatrix);
-	transformation = glm::rotate(0.01f,0.0f,1.0f,0.0f)*transformation*persMatrix;
+	glm::mat4x4 persMatrix = glm::perspective(3.14f*2/3, 1.0f, 0.1f, 0.0f);
+	GLfloat translationMatrix[] = {
+		1.0f,0.0f,0.0f,cameraPosition[0],
+		0.0f,1.0f,0.0f,cameraPosition[1],
+		0.0f,0.0f,1.0f,cameraPosition[2],
+		0.0f,0.0f,0.0f,1.0f
+	};
+	GLfloat rotationMatrix[] = {
+		cos(cameraAngle[0]),0.0f, sin(cameraAngle[0]),0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		-sin(cameraAngle[0]),0.0f,cos(cameraAngle[0]),0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+	GLfloat rotationMatrix2[] = {
+		1.0f,0.0f, 0.0f,0.0f,
+		0.0f,cos(cameraAngle[1]),sin(cameraAngle[1]),0.0f,
+		0.0f,-sin(cameraAngle[1]),cos(cameraAngle[1]),0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+	glm::mat4x4 transMat = glm::make_mat4x4(translationMatrix);
+	glm::mat4x4 rotMat = glm::make_mat4x4(rotationMatrix);
+	glm::mat4x4 rotMat2 = glm::make_mat4x4(rotationMatrix2);
+	glm::mat4x4 transformation =  transMat*rotMat*rotMat2*transpose(persMatrix);
 	glBindVertexArray(array);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, NULL);
-	glUniformMatrix4fv(2, 1, GL_FALSE, glm::value_ptr(transformation));
-	glDrawArrays(GL_TRIANGLES, 0, 15);
+	glUniformMatrix4fv(2, 1, GL_FALSE,glm::value_ptr(transformation));
+	glDrawArrays(GL_TRIANGLES, 0, 9);
 }
 
 
@@ -288,36 +308,48 @@ void handleKeyboardInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
 
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
 	{
-		transmatrix[7] += -0.01f;
+		cameraPosition[1] = cameraPosition[1]-0.01f;
 	}
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
 	{
-		transmatrix[7] += 0.01f;
+		cameraPosition[1] = cameraPosition[1]+0.01f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
 	{
-		transmatrix[3] += 0.01f;
+		cameraPosition[0] = cameraPosition[0]+cos(cameraAngle[0])*0.01f;
+		cameraPosition[2] = cameraPosition[2]+sin(cameraAngle[0])*0.01f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
 	{
-		transmatrix[3] += -0.01f;
+		cameraPosition[0] = cameraPosition[0]-cos(cameraAngle[0])*0.01f;
+		cameraPosition[2] = cameraPosition[2]-sin(cameraAngle[0])*0.01f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+	{
+		cameraPosition[0] = cameraPosition[0]-sin(cameraAngle[0])*0.01f;
+		cameraPosition[2] = cameraPosition[2]+cos(cameraAngle[0])*0.01f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+	{
+		cameraPosition[0] = cameraPosition[0]+sin(cameraAngle[0])*0.01f;
+		cameraPosition[2] = cameraPosition[2]-cos(cameraAngle[0])*0.01f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
 	{
-		transmatrix[11] += 0.01f;
+		cameraAngle[1] = cameraAngle[1]-0.01f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
 	{
-		transmatrix[11] += -0.01f;
+		cameraAngle[1] = cameraAngle[1]+0.01f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
 	{
-
+		cameraAngle[0] = cameraAngle[0]-0.01f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 	{
-		transmatrix[11] += -0.01f;
+		cameraAngle[0] = cameraAngle[0]+0.01f;
 	}
 }
